5f68bc6a559d445ca3b22c7afec671f4e26737bb
ala.biezychudek <ala.biezychudek@zendesk.com>
2024-12-24 01:06:51 +0100

    first draft of adding vehicle

---
+++ build/CMakeFiles/UrbanMotion.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/imgui-src/backends/imgui_impl_glfw.cpp" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/backends/imgui_impl_glfw.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/imgui-src/backends/imgui_impl_opengl3.cpp" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/imgui-src/imgui.cpp" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/imgui-src/imgui_draw.cpp" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui_draw.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui_draw.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/imgui-src/imgui_tables.cpp" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui_tables.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui_tables.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/imgui-src/imgui_widgets.cpp" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui_widgets.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/_deps/imgui-src/imgui_widgets.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/main.cpp" "CMakeFiles/UrbanMotion.dir/main.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/main.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/App.cpp" "CMakeFiles/UrbanMotion.dir/src/App.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/App.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/components/Fields.cpp" "CMakeFiles/UrbanMotion.dir/src/components/Fields.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/components/Fields.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/components/ListOfVehicle.cpp" "CMakeFiles/UrbanMotion.dir/src/components/ListOfVehicle.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/components/ListOfVehicle.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/components/Map.cpp" "CMakeFiles/UrbanMotion.dir/src/components/Map.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/components/Map.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/components/VehicleInMap.cpp" "CMakeFiles/UrbanMotion.dir/src/components/VehicleInMap.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/components/VehicleInMap.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/filestorage/Handler.cpp" "CMakeFiles/UrbanMotion.dir/src/filestorage/Handler.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/filestorage/Handler.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/filestorage/MapFile.cpp" "CMakeFiles/UrbanMotion.dir/src/filestorage/MapFile.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/filestorage/MapFile.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/filestorage/mapper.cpp" "CMakeFiles/UrbanMotion.dir/src/filestorage/mapper.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/filestorage/mapper.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/imgui/DropTargetWindow.cpp" "CMakeFiles/UrbanMotion.dir/src/imgui/DropTargetWindow.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/imgui/DropTargetWindow.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/imgui/GameScreen.cpp" "CMakeFiles/UrbanMotion.dir/src/imgui/GameScreen.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/imgui/GameScreen.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/imgui/Manager.cpp" "CMakeFiles/UrbanMotion.dir/src/imgui/Manager.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/imgui/Manager.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/imgui/Panel.cpp" "CMakeFiles/UrbanMotion.dir/src/imgui/Panel.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/imgui/Panel.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/imgui/WelcomeScreen.cpp" "CMakeFiles/UrbanMotion.dir/src/imgui/WelcomeScreen.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/imgui/WelcomeScreen.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/imgui/Window.cpp" "CMakeFiles/UrbanMotion.dir/src/imgui/Window.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/imgui/Window.cpp.o.d"
+  "/Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/src/texture/Manager.cpp" "CMakeFiles/UrbanMotion.dir/src/texture/Manager.cpp.o" "gcc" "CMakeFiles/UrbanMotion.dir/src/texture/Manager.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/_deps/glm-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/glm-src
+# Build directory: /Users/ala.biezychudek/Code/zendesk/AbstraC/UrbanMotion/build/_deps/glm-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
