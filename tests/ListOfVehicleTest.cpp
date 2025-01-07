efaae949d28a855b417ba6afd50a3430146ca3c5
ala.biezychudek <ala.biezychudek@zendesk.com>
2025-01-07 22:47:52 +0100

    unit test

---
+++ tests/ListOfVehicleTest.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+#include "../src/components/ListOfVehicle.h"
+#include "../src/components/Fields.h"
+#include <memory>
+
+class ListOfVehicleTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        listOfVehicle = std::make_unique<ListOfVehicle>();
+    }
+
+    std::unique_ptr<ListOfVehicle> listOfVehicle;
+};
+
+TEST_F(ListOfVehicleTest, addVehicle) {
+    auto vehicle = std::make_shared<Vehicle>(Texture::ID::Car, 1, 1, 0); // Use a valid Texture::ID
+    listOfVehicle->addVehicle(vehicle);
+    EXPECT_EQ(listOfVehicle->size(), 1);
+}
+
+TEST_F(ListOfVehicleTest, AddDuplicateVehicle) {
+    auto vehicle1 = std::make_shared<Vehicle>(Texture::ID::Car, 1, 0);
+    auto vehicle2 = std::make_shared<Vehicle>(Texture::ID::Car, 1, 0);
+    listOfVehicle->addVehicle(vehicle1);
+    listOfVehicle->addVehicle(vehicle2);
+    EXPECT_EQ(listOfVehicle->size(), 1);
+}
+
+TEST_F(ListOfVehicleTest, RemoveVehicle) {
+    auto vehicle = std::make_shared<Vehicle>(Texture::ID::Car, 1, 0);
+    listOfVehicle->addVehicle(vehicle);
+    listOfVehicle->removeVehicle(1, 1);
+    EXPECT_EQ(listOfVehicle->size(), 0);
+}
+
+TEST_F(ListOfVehicleTest, RemoveNonExistentVehicle) {
+    auto vehicle = std::make_shared<Vehicle>(Texture::ID::Car, 1, 0);
+    listOfVehicle->addVehicle(vehicle);
+    listOfVehicle->removeVehicle(2, 2);
+    EXPECT_EQ(listOfVehicle->size(), 1);
+}
+
+TEST_F(ListOfVehicleTest, PrintAllVehicles) {
+    auto vehicle1 = std::make_shared<Vehicle>(Texture::ID::Car, 1, 0);
+    auto vehicle2 = std::make_shared<Vehicle>(Texture::ID::Car, 2, 0);
+    listOfVehicle->addVehicle(vehicle1);
+    listOfVehicle->addVehicle(vehicle2);
+    testing::internal::CaptureStdout();
+    listOfVehicle->printAllVehicles();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("1, 1"), std::string::npos);
+    EXPECT_NE(output.find("2, 2"), std::string::npos);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
